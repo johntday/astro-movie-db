d57f11ebb9341149f212b3fe6c2d36609cf0c8d7
John Day <johntday@gmail.com>
2024-02-15 10:51:24 -0600

    netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
+    adapter: netlify(),
 });
